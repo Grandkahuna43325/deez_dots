5ed4ebbbd566d4d8334d2f8d162f8c0151ff3266
grandkahuna43325 <kfkorulczyk@gmail.com>
2023-12-06 15:51:14 +0100

    installing plugins...

--- run.sh
+++ run.sh
@@ -15,4 +15,8 @@ cp -r ./.p10k.zsh ~
 cp -r ./.tmux.conf ~
 cp -r ./awesome ~/.config/
 cp -r ./rofi ~/.config/
-cp -r ./nvim ~/.config
+cp -r ./nvim ~/.config/
+
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
