c8d72cd3d39487b35bdd0d037b388d9c6586e802
Grandkahuna43325 <kf.korulczyk@gmail.com>
2023-12-06 15:15:25 +0100

    more fixes...

--- run.sh
+++ run.sh
@@ -3,7 +3,6 @@
 
 rm -fr ~/.zshrc
 rm -fr ~/.tmux.conf
-rm -fr ~/.oh-my-zsh
 rm -fr ~/.p10k.zsh
 rm -fr ~/.tmux.conf
 rm -fr ~/.config/awesome 
@@ -12,7 +11,6 @@ rm -fr ~/.config/nvim
 
 cp -r ./.zshrc ~
 cp -r ./.tmux.conf ~
-cp -r ./.oh-my-zsh ~
 cp -r ./.p10k.zsh ~
 cp -r ./.tmux.conf ~
 cp -r ./awesome ~/.config/
