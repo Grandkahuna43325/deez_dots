596d21aa29869a0edbefa826308f3306004799c4
grandkahuna43325 <kf.korulczyk@gmail.com>
2024-01-06 15:33:28 +0100

    added selection in scripts

--- collect_dots.sh
+++ collect_dots.sh
@@ -1,20 +1,45 @@
 #! /bin/bash
 
-rm -fr ./.zshrc
-rm -fr ./.tmux.conf
-rm -fr ./.p10k.zsh
-rm -fr ./.tmux.conf
-rm -fr ./awesome
-rm -fr ./rofi
-rm -fr ./nvim
+echo "which dots do you want to collect?"
 
+read -p "
+1. zshrc
+2. tmux.conf
+3. p10k.zsh
+4. awesome
+5. rofi
+6. nvim
+"
 
+for i in $REPLY
+do
+	echo "copying $i"
+	case "$i" in
+		1) echo 1 zshrc
+			rm -fr ./.zshrc
+			cp -r ~/.zshrc .
+			;;
+		2) echo 2 tmux.conf
+			rm -fr ./.tmux.conf
+			cp -r ~/.tmux.conf .
+			;;
+		3) echo 3 p10k.zsh
+			rm -fr ./.p10k.zsh
+			cp -r ~/.p10k.zsh .
+			;;
+		4) echo 4 awesome
+			rm -fr ./awesome
+			cp -r ~/.config/awesome  .
+			;;
+		5) echo 5 rofi
+			rm -fr ./rofi
+			cp -r ~/.config/rofi  .
+			;;
+		6) echo 6 nvim
+			rm -fr ./nvim
+			cp -r ~/.config/nvim  .
+			;;
 
-cp -r ~/.zshrc .
-cp -r ~/.tmux.conf .
-cp -r ~/.p10k.zsh .
-cp -r ~/.tmux.conf .
-cp -r ~/.config/awesome  .
-cp -r ~/.config/rofi  .
-cp -r ~/.config/nvim  .
+		esac
+	done
 
--- run.sh
+++ run.sh
@@ -1,5 +1,48 @@
 #! /bin/bash
 
+echo "which dots do you want?"
+
+read -p "
+1. zshrc
+2. tmux.conf
+3. p10k.zsh
+4. awesome
+5. rofi
+6. nvim
+"
+
+for i in $REPLY
+do
+	echo "copying $i"
+	case "$i" in
+		1) echo 1 zshrc
+			rm -fr ~/.zshrc 
+			cp -r ./.zshrc  ~
+			;;
+		2) echo 2 tmux.conf
+			rm -fr ~/.tmux.conf 
+			cp -r ./.tmux.conf ~
+			;;
+		3) echo 3 p10k.zsh
+			rm -fr ~/.p10k.zsh 
+			cp -r ./.p10k.zsh ~
+			;;
+		4) echo 4 awesome
+			rm -fr ~/.config/awesome  
+			cp -r ./awesome ~/.config/
+			;;
+		5) echo 5 rofi
+			rm -fr ~/.config/rofi  
+			cp -r ./rofi ~/.config/
+			;;
+		6) echo 6 nvim
+			rm -fr ~/.config/nvim 
+			cp -r ./nvim ~/.config/
+			;;
+
+		esac
+	done
+
 
 rm -fr ~/.zshrc
 rm -fr ~/.tmux.conf
